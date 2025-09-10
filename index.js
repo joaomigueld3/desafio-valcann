0425b523f5293f14af859cb4d20381ae32ce7abe
João Miguel <joaomigueld3@gmail.com>
2025-09-09 23:49:56 -0300

    chore: nitial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+ 
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Olá, Mundo!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
